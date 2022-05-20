5223085d45001b13be3fa63c953bbfbaa370514a
Yamada Kohki <koki.victory.630@gmail.com>
2022-05-20 16:55:10 +0900

    test

--- Reservation/config/environments/development.rb
+++ Reservation/config/environments/development.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
     address:			'smtp.gmail.com',
     port:			587,
     user_name:			'hamhamsterdragon@gmail.com',
-    password:			'test',
+    password:			'',
     authentication:		:plain,
     enable_starttls_auto:	true
   }
