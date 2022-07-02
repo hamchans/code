1ee0c2d07f84806713cb4c62418c28031abf7fbc
kohki <koki.victory.630@gmail.com>
2022-07-02 06:51:50 +0000

    TCP/IP

---
+++ golang/server/server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func logFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func receiveTCPConnection(listener *net.TCPListener) {
+	for {
+		conn, err := listener.AcceptTCP()
+		logFatal(err)
+
+		echoHandler(conn)
+	}
+}
+
+func echoHandler(conn *net.TCPConn) {
+	defer conn.Close()
+	for {
+		_, err := io.WriteString(conn, "response from server\n")
+		if err != nil {
+			return
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+func main() {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", ":8080")
+	logFatal(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	logFatal(err)
+
+	fmt.Println("Start TCP Server...")
+	fmt.Println(tcpAddr, listener)
+	receiveTCPConnection(listener)
+}
